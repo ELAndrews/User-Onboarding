eb761f1b9080f7dc72dc89ca2a7ad4aa9e1c48b6
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-14 11:52:08 +0000

    Add UserForm render and Yup form valdidation

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withFormik, Form, Field } from 'formik';
+import { withFormik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
@@ -8,27 +8,40 @@ function FormTemplate() {
     return(
         <div>
             <Form>
+                <ErrorMessage 
+                name="name" 
+                render={err => <div className="errorMessage">{err}</div>}/>
                 <label>Name: 
                     <Field 
                         type="text"
+                        name="name"
                         placeholder="Enter your name" 
                         />
                 </label>
+                <ErrorMessage 
+                name="email" 
+                render={err => <div className="errorMessage">{err}</div>}/>
                 <label>Email: 
                     <Field 
                         type="email"
+                        name="email"
                         placeholder="Enter your current email" 
                         />
                 </label>
                 <label>Password: 
                     <Field 
                         type="text"
+                        name="password"
                         placeholder="Password" 
                         />
                 </label>
+                <ErrorMessage 
+                name="terms" 
+                render={err => <div className="errorMessage">{err}</div>}/>
                 <label>Terms of service 
                     <Field 
                         type="checkbox"
+                        name="terms"
                         />
                 </label>
                 <input type="submit" />
@@ -39,6 +52,25 @@ function FormTemplate() {
 }
 
 const UserForm = withFormik({
+    mapPropsToValues() {
+        return {
+            name: "",
+            email: "",
+            password: "",
+            terms: false
+        }
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("Please enter your name"),
+        email: Yup.string().required("Please provide a current email address"),
+        terms: Yup.boolean().required("Do you agree with the Ters of service?")
+    }),
+
+    handleSubmit() {
+
+    }
+
 
 })(FormTemplate)
 
